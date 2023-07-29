f226fb1770f0f65d340e51e30498548f95b2823d
emanlapaz <eugeniojrmanlapaz@gmail.com>
2023-07-29 17:12:01 +0100

    fixed home page

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,38 +1,68 @@
-import React, { useState, useEffect } from "react";
-import PageTemplate from "../components/templateMovieListPage";
-import { getMovies } from "../api/tmdb-api";
-import { useQuery } from "react-query";
-import Spinner from "../components/spinner";
-import AddMovieToFavouritesIcon from '../components/cardIcons/addMovieToFavourites'
-import { getTvShows } from "../api/tmdb-api";
-import AddTvShowToFavouritesIcon from '../components/cardIcons/addTvShowToFavourites'
+import React from "react";
+import { Link } from "react-router-dom";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 
-const HomePage = (props) => {
-  const { data, error, isLoading, isError } = useQuery("discover movies", getMovies,"discover TV shows", getTvShows);
 
-  if (isLoading) {
-    return <Spinner />;
-  }
-  if (isError) {
-    return <h1>{error.message}</h1>;
-  }
+import backgroundImage from "/src/images/pexels-dziana-hasanbekava-5480827.jpg";
 
-  const movies = data ? data.results : [];
-  const tvShows = data ? data.results : [];
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: "100vh",
+    padding: "20px",
+    // Add the background image
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  heading: {
+    marginBottom: "20px",
+  },
+  button: {
+    marginTop: "10px",
+    minWidth: "200px",
+  },
+};
 
+const HomePage = () => {
   return (
-    <PageTemplate
-      title="Home"
-      movies={movies}
-      tvShows={tvShows}
-      action={(movie) => {
-        return <AddMovieToFavouritesIcon movie={movie} />
-        
-      }}
-
-    />
+    <>
+      <Box sx={styles.container}>
+        <Typography variant="h1" sx={styles.heading}>
+          MO-Vision
+        </Typography>
+        <Typography variant="h3" sx={styles.heading}>
+          Welcome to the Movie & TV Show Database!
+        </Typography>
+        <Typography variant="body1" align="center">
+          Explore a vast collection of movies, TV shows, and casts from The Movie Database (TMDb).
+        </Typography>
+        <Stack direction="row" spacing={2} sx={{ marginTop: "20px" }}>
+          <Link to="/movies">
+            <Button variant="contained" color="primary" sx={styles.button}>
+              Browse Movies
+            </Button>
+          </Link>
+          <Link to="/tvShows">
+            <Button variant="contained" color="primary" sx={styles.button}>
+              Browse TV Shows
+            </Button>
+          </Link>
+          <Link to="/casts">
+            <Button variant="contained" color="primary" sx={styles.button}>
+              Explore Casts
+            </Button>
+          </Link>
+        </Stack>
+      </Box>
+    </>
   );
 };
-export default HomePage;
 
-//mix movies and tv shows
+export default HomePage;
