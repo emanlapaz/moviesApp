82125fbe159da085e02f4864765b9f252dc7ab74
emanlapaz <eugeniojrmanlapaz@gmail.com>
2023-08-06 16:59:27 +0100

    add sorting

--- src/components/filterMoviesCard/index.jsx
+++ src/components/filterMoviesCard/index.jsx
@@ -1,17 +1,18 @@
-import React from "react"; 
-//import React, {useState, useEffect}  from "react";
+// FilterMoviesCard.js
+
+import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import TextField from "@mui/material/TextField";
-import FilterAltIcon from '@mui/icons-material/FilterAlt';
-import SortIcon from '@mui/icons-material/Sort';
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import SortIcon from "@mui/icons-material/Sort";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import { useQuery } from "react-query";
-import Spinner from '../spinner'
+import Spinner from "../spinner";
 import { getMovieGenres } from "../../api/tmdb-api";
 
 const styles = {
@@ -37,32 +38,38 @@ export default function FilterMoviesCard(props) {
   if (isError) {
     return <h1>{error.message}</h1>;
   }
+
   const genres = data.genres;
   if (genres[0].name !== "All") {
     genres.unshift({ id: "0", name: "All" });
   }
 
-  const handleUserImput = (e, type, value) => {
+  const handleUserInput = (e, type, value) => {
     e.preventDefault();
-    props.onUserInput(type, value); // NEW
+    props.onUserInput(type, value);
   };
 
-  const handleTextChange = (e, props) => {
-    handleUserImput(e, "title", e.target.value);
+  const handleTextChange = (e) => {
+    handleUserInput(e, "title", e.target.value);
   };
 
   const handleGenreChange = (e) => {
-    handleUserImput(e, "genre", e.target.value);
+    handleUserInput(e, "genre", e.target.value);
   };
+
+  const handleSortChange = (e) => {
+    handleUserInput(e, "sort", e.target.value);
+  };
+
   return (
     <>
-    <Card sx={styles.root} variant="outlined">
-      <CardContent>
-        <Typography variant="h5" component="h1">
-          <FilterAltIcon fontSize="large" />
-          Filter
-        </Typography>
-        <TextField
+      <Card sx={styles.root} variant="outlined">
+        <CardContent>
+          <Typography variant="h5" component="h1">
+            <FilterAltIcon fontSize="large" />
+            Filter
+          </Typography>
+          <TextField
             sx={styles.formControl}
             id="filled-search"
             label="Search field"
@@ -70,34 +77,50 @@ export default function FilterMoviesCard(props) {
             value={props.titleFilter}
             variant="filled"
             onChange={handleTextChange}
-            />
-        <FormControl sx={styles.formControl}>
-          <InputLabel id="genre-label">Genre</InputLabel>
-          <Select
-            labelId="genre-label"
-            id="genre-select"
-            value={props.genreFilter}
-            onChange={handleGenreChange}
+          />
+          <FormControl sx={styles.formControl}>
+            <InputLabel id="genre-label">Genre</InputLabel>
+            <Select
+              labelId="genre-label"
+              id="genre-select"
+              value={props.genreFilter}
+              onChange={handleGenreChange}
             >
-            {genres.map((genre) => {
-              return (
+              {genres.map((genre) => (
                 <MenuItem key={genre.id} value={genre.id}>
                   {genre.name}
                 </MenuItem>
-              );
-            })}
-          </Select>
-        </FormControl>
-      </CardContent>
-    </Card>
-    <Card sx={styles.root} variant="outlined">
+              ))}
+            </Select>
+          </FormControl>
+        </CardContent>
+      </Card>
+
+      <Card sx={styles.root} variant="outlined">
         <CardContent>
           <Typography variant="h5" component="h1">
             <SortIcon fontSize="large" />
             Sort the movies.
           </Typography>
+          <br/>
+          <FormControl sx={styles.formControl}>
+            <InputLabel id="sort-label">Sort By</InputLabel>
+            <Select
+              labelId="sort-label"
+              id="sort-select"
+              value={props.sortFilter}
+              onChange={handleSortChange}
+            >
+              <MenuItem value={"popularity.desc"}>Popularity Descending</MenuItem>
+              <MenuItem value={"popularity.asc"}>Popularity Ascending</MenuItem>
+              <MenuItem value={"vote_average.desc"}>Rating Descending</MenuItem>
+              <MenuItem value={"vote_average.asc"}>Rating Ascending</MenuItem>
+              <MenuItem value={"release_date.desc"}>Release Date Descending</MenuItem>
+              <MenuItem value={"release_date.asc"}>Release Date Ascending</MenuItem>
+            </Select>
+          </FormControl>
         </CardContent>
       </Card>
-      </>
+    </>
   );
 }
--- src/components/templateMovieListPage/index.jsx
+++ src/components/templateMovieListPage/index.jsx
@@ -6,6 +6,7 @@ import Fab from "@mui/material/Fab";
 import Drawer from "@mui/material/Drawer";
 import MovieList from "../movieList";
 import { getMovies } from "../../api/tmdb-api";
+import TextField from "@mui/material/TextField";
 
 const styles = {
   container: {
@@ -30,18 +31,20 @@ const styles = {
   },
 };
 
-
 function MovieListPageTemplate({ title, action }) {
   const [movies, setMovies] = useState([]);
   const [titleFilter, setTitleFilter] = useState("");
   const [genreFilter, setGenreFilter] = useState("0");
+  const [releaseYearFilter, setReleaseYearFilter] = useState("");
+  const [ratingFilter, setRatingFilter] = useState("");
+  const [sortFilter, setSortFilter] = useState("popularity.desc");
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [drawerOpen, setDrawerOpen] = useState(false);
 
   useEffect(() => {
     fetchMovies();
-  }, [currentPage]);
+  }, [currentPage, sortFilter]);
 
   const handlePageChange = (newPage) => {
     if (newPage >= 1 && newPage <= totalPages) {
@@ -52,16 +55,41 @@ function MovieListPageTemplate({ title, action }) {
   const fetchMovies = async () => {
     try {
       const data = await getMovies(currentPage);
-      setMovies(data.results);
+      const sortedMovies = sortMovies(data.results, sortFilter);
+      setMovies(sortedMovies);
       setTotalPages(data.total_pages);
     } catch (error) {
       console.error(error.message);
     }
   };
 
+  const sortMovies = (movies, sortFilter) => {
+    // Implement sorting logic here
+    // For example, you can use JavaScript's Array.sort() method
+    // Here's a basic example for sorting by popularity:
+    return [...movies].sort((a, b) => {
+      if (sortFilter === "popularity.desc") {
+        return b.popularity - a.popularity;
+      } else if (sortFilter === "popularity.asc") {
+        return a.popularity - b.popularity;
+      }
+      // Add other sorting cases as needed
+      return 0;
+    });
+  };
+
   const handleChange = (type, value) => {
-    if (type === "title") setTitleFilter(value);
-    else setGenreFilter(value);
+    if (type === "title") {
+      setTitleFilter(value);
+    } else if (type === "genre") {
+      setGenreFilter(value);
+    } else if (type === "releaseYear") {
+      setReleaseYearFilter(value);
+    } else if (type === "rating") {
+      setRatingFilter(value);
+    } else if (type === "sort") {
+      setSortFilter(value);
+    }
   };
 
   const displayedMovies = movies
@@ -71,6 +99,13 @@ function MovieListPageTemplate({ title, action }) {
     .filter((m) => {
       const genreId = Number(genreFilter);
       return genreId > 0 ? m.genre_ids.includes(genreId) : true;
+    })
+    .filter((m) => {
+      return !releaseYearFilter || (m.release_date && m.release_date.includes(releaseYearFilter));
+    })
+    .filter((m) => {
+      const rating = Number(ratingFilter);
+      return !ratingFilter || (m.vote_average && m.vote_average >= rating);
     });
 
   return (
@@ -87,7 +122,14 @@ function MovieListPageTemplate({ title, action }) {
         Filter
       </Fab>
       <Drawer anchor="left" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
-        <FilterCard onUserInput={handleChange} titleFilter={titleFilter} genreFilter={genreFilter} />
+        <FilterCard
+          onUserInput={handleChange}
+          titleFilter={titleFilter}
+          genreFilter={genreFilter}
+          releaseYearFilter={releaseYearFilter}
+          ratingFilter={ratingFilter}
+          sortFilter={sortFilter}
+        />
       </Drawer>
     </div>
   );
